c4993de36ad107376bf74b7ac579f5f52a2f603b
babymonkey1111 <babymonkey111@outlook.com>
2023-11-10 13:44:33 +0100

    Update models.py

--- base/models.py
+++ base/models.py
@@ -10,5 +10,5 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     
 
-def __str__(self):
-    return self.body[0:50]
+    def __str__(self):
+        return self.body[0:50]
